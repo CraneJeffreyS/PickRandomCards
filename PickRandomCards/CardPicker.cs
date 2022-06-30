8ebcf6345e3e2efb93681ebf36a3116266c9a4f2
Jeffrey Crane <crane.jeffrey.s@gmail.com>
2022-06-30 15:41:59 -0700

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuite();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            switch (value)
+            {
+                case 1: return "Ace";
+                case 10: return "Jack";
+                case 11: return "Queen";
+                case 12: return "King";
+                default: return value.ToString();
+            }
+        }
+
+        private static string RandomSuite()
+        {
+            int value = random.Next(1, 5);
+            switch (value)
+            {
+                case 1: return "Spades";
+                case 2: return "Diamonds";
+                case 3: return "Hearts";
+                default: return "Clubs";
+            }
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many cards to pick?");
+            string qty = Console.ReadLine();
+            if (int.TryParse(qty, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("That wasn't a number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
